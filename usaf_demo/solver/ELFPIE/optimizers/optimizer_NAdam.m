2e5cbc711a504079d04a83d234a6d1339910d970
Shuhe Zhang <61624968+ShuheZhang-MUMC@users.noreply.github.com>
2023-05-01 15:32:10 +0200

    Add files via upload

---
+++ usaf_demo/solver/ELFPIE/optimizers/optimizer_NAdam.m
@@ -0,0 +1,15 @@
+function o_new = optimizer_NAdam(o_old,r1,r2,loop)
+    o_new = o_old;
+    
+    eps_ = 1e-5;
+
+    o_new.mom1 = r1 * o_old.mom1 + (1 - r1) * o_old.grad;
+    o_new.mom2 = r2 * o_old.mom2 + (1 - r2) * abs(o_old.grad).^2;
+        
+    m1 = o_new.mom1/(1 - r1^loop);
+    m2 = o_new.mom2/(1 - r2^loop);
+
+    new_grad =  (r1 * m1 + (1 - r1) * o_old.grad)./ (sqrt(m2) + eps_);
+
+    o_new.value = o_old.value - o_new.step .* new_grad; 
+end
